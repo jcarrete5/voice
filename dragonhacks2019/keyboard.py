755afaeb6f51c35b1829482a39304d53f06c0c0a
juvenal <jarellanobooking@gmail.com>
2019-04-20 16:38:44 -0400

    funnctions added

--- dragonhacks2019/keyboard.py
+++ dragonhacks2019/keyboard.py
@@ -1,3 +1,12 @@
 from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
+
+def forward():
+    keyboard.press(Key.right)
+    keyboard.release(Key.right)
+
+def backward():
+    keyboard.press(Key.left)
+    keyboard.release(Key.left)
+
