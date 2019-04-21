22de21309c72ecb233c3e252bfa1d2a006d874b5
Zach <zachmatuson@gmail.com>
2019-04-20 21:28:35 -0400

    Add close methods and phrase hint updates

--- dragonhacks2019/speech_to_text.py
+++ dragonhacks2019/speech_to_text.py
@@ -3,6 +3,7 @@ from google.cloud.speech_v1 import enums
 from google.cloud.speech_v1 import types
 import pyaudio
 import pathlib
+import time
 
 from microphone_input import MicrophoneInput
 
@@ -10,14 +11,19 @@ class SpeechToTextClient:
     """
     A speech to text client that parses microphone input into text.
     """
-    def __init__(self, credentials_path, language_code):
+    def __init__(self, credentials_path, language_code, phrase_hints=[]):
         """
         Args:
             credentials_path (str): The path to the service account private key json file.
             language_code (str): The language of the supplied audio as a BCP-47 language tag. Example: “en-US”.
+            phrase_hints (str[]): https://cloud.google.com/speech-to-text/docs/basics#phrase-hints
         """
         self.language_code = language_code
+
         self.client = SpeechClient().from_service_account_json(credentials_path)
+        self.phrase_hints = phrase_hints
+
+        self._mic: MicrophoneInput = None
 
     def start(self, callback):
         """
@@ -25,17 +31,20 @@ class SpeechToTextClient:
             callback (function): Function that is called when text is transcribed from speech
         """
         with MicrophoneInput() as mic:
-            audio_generator = mic.generator()
+            print("Starting SpeechToTextClient")
+            self._mic = mic
+            audio_generator = self._mic.generator()
 
             config = types.RecognitionConfig(
                     encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-                    sample_rate_hertz=mic.RATE,
+                    sample_rate_hertz=self._mic.RATE,
                     language_code=self.language_code,
                     use_enhanced=True
+                    # speech_contexts=self.phrase_hints
             )
 
-            streaming_config = types.StreamingRecognitionConfig(config=config)
-                                                                # interim_results=True)
+            streaming_config = types.StreamingRecognitionConfig(config=config,
+                                                                interim_results=True)
 
             requests = (types.StreamingRecognizeRequest(audio_content=content) for content in audio_generator)
             responses = self.client.streaming_recognize(streaming_config, requests)
@@ -50,7 +59,23 @@ class SpeechToTextClient:
                     continue
 
                 transcript = result.alternatives[0].transcript.strip()
+                print (response)
                 callback(transcript)
+
+    def close(self):
+        print("Stopping SpeechToTextClient")
+        if self._mic is None or self._mic.closed:
+            return
+
+        self._mic.close()
+
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def update_phrase_hints(self, phrase_hints):
+        self.phrase_hints = phrase_hints
 
 
 if __name__ == "__main__":
@@ -60,5 +85,7 @@ if __name__ == "__main__":
     def callback(text):
         print(text)
 
-    client = SpeechToTextClient(cred_path, "en-US")
+    client = SpeechToTextClient(cred_path, "en-US", ["ChromeCast"])
     client.start(callback)
+    time.sleep(2)
+    client.stop()
