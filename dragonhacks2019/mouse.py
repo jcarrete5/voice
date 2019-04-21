4ddacc4542a58d7995d7af8754e610a36ca9e2fe
juvenal <jarellanobooking@gmail.com>
2019-04-21 11:02:15 -0400

    owO

--- dragonhacks2019/mouse.py
+++ dragonhacks2019/mouse.py
@@ -1,4 +1,3 @@
-#from pynput.mouse import Button, Controller
 import pynput.mouse as ms
 import time
 import tkinter as tk
@@ -6,30 +5,24 @@ import tkinter as tk
 
 
 mouse = ms.Controller()
-
 root=tk.Tk()
 
-def newCoordiantes():
+def newCoordinates():
     Screen_Width= root.winfo_screenwidth()
     Screen_Height=root.winfo_screenheight()
-
     x= 1690/1920 *Screen_Width
     y=100/1080*Screen_Height
-    newCoordiantes = (x, y)
-    return newCoordiantes
+    newCoordinates = (x, y)
+    return newCoordinates
 
 def open():
-
-
     coordinates= mouse.position
-    x= coordinates[0]
-    y= coordinates[1]
-    presentButtonCoordinates= newCoordiantes()
-
-    xMovement= presentButtonCoordinates[0]-x
-    yMovement=presentButtonCoordinates[1]-y
+    x = coordinates[0]
+    y = coordinates[1]
+    presentButtonCoordinates= newCoordinates()
+    xMovement = presentButtonCoordinates[0]-x
+    yMovement =presentButtonCoordinates[1]-y
     mouse.move(xMovement,yMovement)
-
     mouse.click(ms.Button.left, 1)
 
 if __name__ == '__main__':
